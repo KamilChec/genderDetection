bd4b52a718905d40c1a13ce0418bcdc4b4e60f85
KamilChec <k.chec.prv@gmail.com>
2019-06-10 21:52:54 +0200

    add 3 differet classifiers

--- gender.py
+++ gender.py
@@ -1,7 +1,19 @@
 import numpy as np
 import itertools
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
+
+
+def classifier(model, x, y, test_data, ture_data):
+    model.fit(x, y)
+    prediction = model.predict(test_data)
+    scores = cross_val_score(model, x, y, cv=5)
+    print("Accuracy score: {}".format(accuracy_score(ture_data, prediction)))
+    print('Cross validation accuracy: %0.2f (+/- %0.2f)' % (scores.mean(), scores.std() * 2))
+
 
 x = []
 y = []
@@ -17,10 +29,8 @@ with open(r'C:\Users\Kamil\Desktop\labSpec2\temp.csv', 'r') as file:
         del fields[230]
         del fields[259]
         x.append(list(map(float, fields[2:-1])))
-
-clf = LinearDiscriminantAnalysis()
-clf.fit(x, y)
-
+xNp = np.asarray(x)
+yNp = np.asarray(y)
 true = []
 test = []
 
@@ -35,6 +45,15 @@ with open(r'C:\Users\Kamil\Desktop\labSpec2\temp.csv', 'r') as file:
         del fields[230]
         del fields[259]
         test.append(list(map(float, fields[2:-1])))
-prediction = clf.predict(test)
-print(accuracy_score(true, prediction))
-print(accuracy_score(true, prediction, normalize=False))
+testNp = np.asarray(test)
+trueNp = np.asarray(true)
+lda = LinearDiscriminantAnalysis()
+lr = LogisticRegression()
+svm = SVC()
+
+print("LinearDiscriminantAnalysis" + '-'*20)
+classifier(lda, xNp, yNp, testNp, trueNp)
+print("LogisticRegression" + '-'*20)
+classifier(lr, xNp, yNp, testNp, trueNp)
+print("SVC" + '-'*20)
+classifier(svm, xNp, yNp, testNp, trueNp)
